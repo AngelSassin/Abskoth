2357e26ec4196bac87b83beb4786b1f6e0a5f0f7
Alprechim <hdploeanix@gmail.com>
2023-08-18 03:54:36 +0200

    added mod toggle

--- Abskoth.cs
+++ Abskoth.cs
@@ -7,233 +7,241 @@ using HutongGames.PlayMaker;
 
 namespace Abskoth
 {
-    public partial class Abskoth : Mod
-    {
-        public const string Version = "1.0.0.0";
-        public override string GetVersion() => Abskoth.Version;
-        internal static Abskoth Instance;
-        internal bool inAbskothFight = false;
-        internal int AbskothPhase = 0;
+	public partial class Abskoth : Mod, ITogglableMod
+	{
+		public const string Version = "1.0.0.1";
+		public override string GetVersion() => Abskoth.Version;
+		internal static Abskoth Instance;
+		internal bool inAbskothFight = false;
+		internal int AbskothPhase = 0;
+		internal GameObject markothPrefab = null;
+		private const string _scene = "Deepnest_East_16";
+		private const string _obj = "Warrior/Ghost Warrior Markoth";
 
-        internal GameObject markothPrefab = null;
-        private const string _scene = "Deepnest_East_16";
-        private const string _obj = "Warrior/Ghost Warrior Markoth";
+		public override List<(string, string)> GetPreloadNames() => new List<(string, string)> { (_scene, _obj) };
 
-        public override List<(string, string)> GetPreloadNames() => new List<(string, string)> { (_scene, _obj) };
+		public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+		{
+			Instance = this;
 
-        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
-        {
-            Instance = this;
+			if (preloadedObjects != null)
+				markothPrefab = preloadedObjects[_scene][_obj];
+			GameObject.DontDestroyOnLoad(markothPrefab);
 
-            if (preloadedObjects != null)
-                markothPrefab = preloadedObjects[_scene][_obj];
-            GameObject.DontDestroyOnLoad(markothPrefab);
+			ModHooks.OnEnableEnemyHook += EnemyEnabled;
+			ModHooks.BeforeSceneLoadHook += OnSceneLoad;
+			ModHooks.ObjectPoolSpawnHook += OnObjectSpawn;
+			
+			On.HealthManager.TakeDamage += OnTakeDamage;
+			On.HutongGames.PlayMaker.Actions.RandomFloat.OnEnter += OnRandomFloat;
+		}
+		public void Unload() 
+		{
+			ModHooks.OnEnableEnemyHook -= EnemyEnabled;
+			ModHooks.BeforeSceneLoadHook -= OnSceneLoad;
+			ModHooks.ObjectPoolSpawnHook -= OnObjectSpawn;
+			
+			On.HealthManager.TakeDamage -= OnTakeDamage;
+			On.HutongGames.PlayMaker.Actions.RandomFloat.OnEnter -= OnRandomFloat;
+		}
 
-            ModHooks.OnEnableEnemyHook += EnemyEnabled;
-            ModHooks.BeforeSceneLoadHook += OnSceneLoad;
-            ModHooks.ObjectPoolSpawnHook += OnObjectSpawn;
-            
-            On.HealthManager.TakeDamage += OnTakeDamage;
-            On.HutongGames.PlayMaker.Actions.RandomFloat.OnEnter += OnRandomFloat;
-        }
+		private GameObject OnObjectSpawn(GameObject arg)
+		{
+			String name = arg.name;
+			if (name.Contains("("))
+				name = name.Substring(0, arg.name.IndexOf("(")).Trim();
+			if (!name.Equals("Shot Markoth Nail"))
+				return arg;
 
-        private GameObject OnObjectSpawn(GameObject arg)
-        {
-            String name = arg.name;
-            if (name.Contains("("))
-                name = name.Substring(0, arg.name.IndexOf("(")).Trim();
-            if (!name.Equals("Shot Markoth Nail"))
-                return arg;
+			if (inAbskothFight)
+			{
+				if (!GlobalSaveData.withNail)
+				{
+					GameObject.Destroy(arg);
+					return null;
+				}
 
-            if (inAbskothFight)
-            {
-                if (!GlobalSaveData.withNail)
-                {
-                    GameObject.Destroy(arg);
-                    return null;
-                }
+				PlayMakerFSM fsm = arg.LocateMyFSM("Control");
+				foreach (FsmFloat f in fsm.FsmVariables.FloatVariables)
+					f.Value = GetFValue(f);
+			}
 
-                PlayMakerFSM fsm = arg.LocateMyFSM("Control");
-                foreach (FsmFloat f in fsm.FsmVariables.FloatVariables)
-                    f.Value = GetFValue(f);
-            }
+			return arg;
+		}
 
-            return arg;
-        }
+		private void OnRandomFloat(On.HutongGames.PlayMaker.Actions.RandomFloat.orig_OnEnter orig, HutongGames.PlayMaker.Actions.RandomFloat self)
+		{
+			String name = self.Fsm.GameObject.name;
+			Vector3 pos = HeroController.instance.transform.position;
 
-        private void OnRandomFloat(On.HutongGames.PlayMaker.Actions.RandomFloat.orig_OnEnter orig, HutongGames.PlayMaker.Actions.RandomFloat self)
-        {
-            String name = self.Fsm.GameObject.name;
-            Vector3 pos = HeroController.instance.transform.position;
+			if (name.Contains("("))
+				name = name.Substring(0, self.Fsm.GameObject.name.IndexOf("(")).Trim();
+			if (name.Equals("Shot Markoth Nail") && self.Fsm.Name.Equals("Control"))
+			{
+				if (self.max.Value == 43f || self.max.Value == 72.5f || self.max.Value == 80.0f || self.max.Value == 75.0f)
+					self.min.Value = AbskothPhase == 0 ? 48.5F : AbskothPhase == 1 ? 40.0F : 51.0F;
+				else self.min.Value = pos.y < 34.0F ? 19.0F : pos.y < 58.0F ? 33.0F : pos.y < 152.0F ? 54.0F : 152.0F;
+			}
 
-            if (name.Contains("("))
-                name = name.Substring(0, self.Fsm.GameObject.name.IndexOf("(")).Trim();
-            if (name.Equals("Shot Markoth Nail") && self.Fsm.Name.Equals("Control"))
-            {
-                if (self.max.Value == 43f || self.max.Value == 72.5f || self.max.Value == 80.0f || self.max.Value == 75.0f)
-                    self.min.Value = AbskothPhase == 0 ? 48.5F : AbskothPhase == 1 ? 40.0F : 51.0F;
-                else self.min.Value = pos.y < 34.0F ? 19.0F : pos.y < 58.0F ? 33.0F : pos.y < 152.0F ? 54.0F : 152.0F;
-            }
+			orig(self);
+		}
 
-            orig(self);
-        }
+		private float GetFValue(FsmFloat f)
+		{
+			if (f.Name.Equals("X Max")) // 48.5 72.5     40.0 80.0     51.0 75.0
+				f.Value = AbskothPhase == 0 ? 72.5F : AbskothPhase == 1 ? 80.0F : 75.0F;
+			if (f.Name.Equals("Y Max")) // 19.0 32.0     33.0 56.0     152.0 163.5               150.0 for climb
+				f.Value = AbskothPhase == 0 ? 32.0F : AbskothPhase == 1 ? 56.0F : HeroController.instance.transform.position.y >= 152.0F ? 150.0F : 163.5F;
 
-        private float GetFValue(FsmFloat f)
-        {
-            if (f.Name.Equals("X Max")) // 48.5 72.5     40.0 80.0     51.0 75.0
-                f.Value = AbskothPhase == 0 ? 72.5F : AbskothPhase == 1 ? 80.0F : 75.0F;
-            if (f.Name.Equals("Y Max")) // 19.0 32.0     33.0 56.0     152.0 163.5               150.0 for climb
-                f.Value = AbskothPhase == 0 ? 32.0F : AbskothPhase == 1 ? 56.0F : HeroController.instance.transform.position.y >= 152.0F ? 150.0F : 163.5F;
-
-            return f.Value;
-        }
+			return f.Value;
+		}
 
 
 
-        private void OnTakeDamage(On.HealthManager.orig_TakeDamage orig, HealthManager self, HitInstance hitInstance)
-        {
-            String name = self.gameObject.name;
-            if (name.Contains("("))
-                name = name.Substring(0, self.gameObject.name.IndexOf("(")).Trim();
+		private void OnTakeDamage(On.HealthManager.orig_TakeDamage orig, HealthManager self, HitInstance hitInstance)
+		{
+			String name = self.gameObject.name;
+			if (name.Contains("("))
+				name = name.Substring(0, self.gameObject.name.IndexOf("(")).Trim();
 
-            if (inAbskothFight && name.Equals("Ghost Warrior Markoth"))
-                hitInstance.DamageDealt = 0;
+			if (inAbskothFight && name.Equals("Ghost Warrior Markoth"))
+				hitInstance.DamageDealt = 0;
 
-            orig(self, hitInstance);
-        }
+			orig(self, hitInstance);
+		}
 
-        private string OnSceneLoad(string arg)
-        {
-            inAbskothFight = false;
-            return arg;
-        }
+		private string OnSceneLoad(string arg)
+		{
+			inAbskothFight = false;
+			return arg;
+		}
 
-        public bool EnemyEnabled(GameObject enemy, bool isDead)
-        {
-            if (isDead)
-                return isDead;
+		public bool EnemyEnabled(GameObject enemy, bool isDead)
+		{
+			if (isDead)
+				return isDead;
 
-            if (enemy.name.Equals("Absolute Radiance"))
-            {
-                inAbskothFight = true;
-                AbskothPhase = 0;
-                if (markothPrefab != null)
-                    GameManager.instance.StartCoroutine(SpawnMarkoth(enemy));
-                else Log("No Markoth Prefab to spawn.");
-            }
+			if (enemy.name.Equals("Absolute Radiance"))
+			{
+				inAbskothFight = true;
+				AbskothPhase = 0;
+				if (markothPrefab != null)
+					GameManager.instance.StartCoroutine(SpawnMarkoth(enemy));
+				else Log("No Markoth Prefab to spawn.");
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        IEnumerator SpawnMarkoth(GameObject enemy)
-        {
-            yield return new WaitForSeconds(1.6f);
+		IEnumerator SpawnMarkoth(GameObject enemy)
+		{
+			yield return new WaitForSeconds(1.6f);
 
-            Log("Spawning " + Abskoth.GlobalSaveData.numMarkoths + " Markoths!");
-            for (int i = 0; i < Abskoth.GlobalSaveData.numMarkoths; i++)
-            {
-                Log("Markoth: " + markothPrefab);
-                GameObject go = GameObject.Instantiate(markothPrefab);
-                go.SetActive(true);
+			Log("Spawning " + Abskoth.GlobalSaveData.numMarkoths + " Markoths!");
+			for (int i = 0; i < Abskoth.GlobalSaveData.numMarkoths; i++)
+			{
+				Log("Markoth: " + markothPrefab);
+				GameObject go = GameObject.Instantiate(markothPrefab);
+				go.SetActive(true);
 
-                switch (i) {
-                    case 0: go.transform.position = new Vector3(52, HeroController.instance.transform.position.y + 5, -1); break;
-                    case 1: go.transform.position = new Vector3(68.7F, HeroController.instance.transform.position.y + 5, -1); break;
-                    case 2: go.transform.position = new Vector3(44, HeroController.instance.transform.position.y + 5, -1); break;
-                    case 3: go.transform.position = new Vector3(76.7F, HeroController.instance.transform.position.y + 5, -1); break;
-                }
+				switch (i) {
+					case 0: go.transform.position = new Vector3(52, HeroController.instance.transform.position.y + 5, -1); break;
+					case 1: go.transform.position = new Vector3(68.7F, HeroController.instance.transform.position.y + 5, -1); break;
+					case 2: go.transform.position = new Vector3(44, HeroController.instance.transform.position.y + 5, -1); break;
+					case 3: go.transform.position = new Vector3(76.7F, HeroController.instance.transform.position.y + 5, -1); break;
+				}
 
-                go.AddComponent<AbskothBehaviour>();
+				go.AddComponent<AbskothBehaviour>();
 
-                foreach (FsmBool b in go.LocateMyFSM("Shield Attack").FsmVariables.BoolVariables)
-                {
-                    if (b.Name.Equals("Rage"))
-                        b.Value = true;
-                }
-                foreach (FsmBool b in go.LocateMyFSM("Attacking").FsmVariables.BoolVariables)
-                {
-                    if (b.Name.Equals("Rage"))
-                        b.Value = true;
-                }
-            }
-        }
-    }
-    public class AbskothBehaviour : MonoBehaviour
-    {
-        private int phase = 0;
-        private bool update = false;
-        private GameObject absrad;
-        private HealthManager hm;
-        private PlayMakerFSM fsm;
-        private float[] phaseUp = { 0, 25, 133 };
-        private float[] phaseDown = { 0, 17, 133 };
-        private float[] phaseLeft = { 0, -6, 8 };
-        private float[] phaseRight = { 0, 4, 2 };
+				foreach (FsmBool b in go.LocateMyFSM("Shield Attack").FsmVariables.BoolVariables)
+				{
+					if (b.Name.Equals("Rage"))
+						b.Value = true;
+				}
+				foreach (FsmBool b in go.LocateMyFSM("Attacking").FsmVariables.BoolVariables)
+				{
+					if (b.Name.Equals("Rage"))
+						b.Value = true;
+				}
+			}
+		}
+	}
+	public class AbskothBehaviour : MonoBehaviour
+	{
+		private int phase = 0;
+		private bool update = false;
+		private GameObject absrad;
+		private HealthManager hm;
+		private PlayMakerFSM fsm;
+		private float[] phaseUp = { 0, 25, 133 };
+		private float[] phaseDown = { 0, 17, 133 };
+		private float[] phaseLeft = { 0, -6, 8 };
+		private float[] phaseRight = { 0, 4, 2 };
 
-        public void Awake()
-        {
-            phase = 0;
-            update = false;
-            absrad = GameObject.Find("Absolute Radiance");
-            hm = absrad.GetComponent<HealthManager>();
-            fsm = absrad.LocateMyFSM("Control");
+		public void Awake()
+		{
+			phase = 0;
+			update = false;
+			absrad = GameObject.Find("Absolute Radiance");
+			hm = absrad.GetComponent<HealthManager>();
+			fsm = absrad.LocateMyFSM("Control");
 
-            foreach (FsmVector3 v in this.gameObject.LocateMyFSM("Movement").FsmVariables.Vector3Variables)
-            {
-                switch (v.Name)
-                {
-                    case "P1": v.Value = new Vector2(60.00F, 28.00F); break;
-                    case "P2": v.Value = new Vector2(60.00F, 23.00F); break;
-                    case "P3": v.Value = new Vector2(48.50F, 21.00F); break;
-                    case "P4": v.Value = new Vector2(72.50F, 21.00F); break;
-                    case "P5": v.Value = new Vector2(72.10F, 27.50F); break;
-                    case "P6": v.Value = new Vector2(48.80F, 27.50F); break;
-                    case "P7": v.Value = new Vector2(55.50F, 25.00F); break;
-                    case "P8": v.Value = new Vector2(64.50F, 25.00F); break;
-                }
-            }
-        }
+			foreach (FsmVector3 v in this.gameObject.LocateMyFSM("Movement").FsmVariables.Vector3Variables)
+			{
+				switch (v.Name)
+				{
+					case "P1": v.Value = new Vector2(60.00F, 28.00F); break;
+					case "P2": v.Value = new Vector2(60.00F, 23.00F); break;
+					case "P3": v.Value = new Vector2(48.50F, 21.00F); break;
+					case "P4": v.Value = new Vector2(72.50F, 21.00F); break;
+					case "P5": v.Value = new Vector2(72.10F, 27.50F); break;
+					case "P6": v.Value = new Vector2(48.80F, 27.50F); break;
+					case "P7": v.Value = new Vector2(55.50F, 25.00F); break;
+					case "P8": v.Value = new Vector2(64.50F, 25.00F); break;
+				}
+			}
+		}
 
-        public void FixedUpdate()
-        {
-            CheckAbsradHealth();
-            if (update)
-                UpdatePosition();
-        }
+		public void FixedUpdate()
+		{
+			CheckAbsradHealth();
+			if (update)
+				UpdatePosition();
+		}
 
-        private void CheckAbsradHealth()
-        {
-            int hp = hm.hp;
-            if (hp > 1850)
-                return;
-            if (hp > 1100 && phase == 1)
-                return;
-            if (fsm.ActiveStateName.Equals("Knight Break Antic"))
-                GameObject.Destroy(this.gameObject);
-            if (hp <= 1100 && phase == 2)
-                return;
-            phase += 1;
-            Abskoth.Instance.AbskothPhase = phase;
-            update = true;
-        }
+		private void CheckAbsradHealth()
+		{
+			int hp = hm.hp;
+			if (hp > 1850)
+				return;
+			if (hp > 1100 && phase == 1)
+				return;
+			if (fsm.ActiveStateName.Equals("Knight Break Antic"))
+				GameObject.Destroy(this.gameObject);
+			if (hp <= 1100 && phase == 2)
+				return;
+			phase += 1;
+			Abskoth.Instance.AbskothPhase = phase;
+			update = true;
+		}
 
-        private void UpdatePosition()
-        {
-            update = false;
-            foreach (FsmVector3 v in this.gameObject.LocateMyFSM("Movement").FsmVariables.Vector3Variables)
-            {
-                switch (v.Name)
-                {
-                    case "P1": v.Value = new Vector2(60.00F, 28.00F + phaseUp[phase]); break;
-                    case "P2": v.Value = new Vector2(60.00F, 23.00F + phaseDown[phase]); break;
-                    case "P3": v.Value = new Vector2(48.50F + phaseLeft[phase], 21.00F + phaseDown[phase]); break;
-                    case "P4": v.Value = new Vector2(72.50F + phaseRight[phase], 21.00F + phaseDown[phase]); break;
-                    case "P5": v.Value = new Vector2(72.10F + phaseRight[phase], 27.50F + phaseUp[phase]); break;
-                    case "P6": v.Value = new Vector2(48.80F + phaseLeft[phase], 27.50F + phaseUp[phase]); break;
-                    case "P7": v.Value = new Vector2(55.50F + (phaseLeft[phase]/2), 25.00F + phaseUp[phase]); break;
-                    case "P8": v.Value = new Vector2(64.50F + (phaseRight[phase]/2), 25.00F + phaseUp[phase]); break;
-                }
-            }
-        }
-    }
+		private void UpdatePosition()
+		{
+			update = false;
+			foreach (FsmVector3 v in this.gameObject.LocateMyFSM("Movement").FsmVariables.Vector3Variables)
+			{
+				switch (v.Name)
+				{
+					case "P1": v.Value = new Vector2(60.00F, 28.00F + phaseUp[phase]); break;
+					case "P2": v.Value = new Vector2(60.00F, 23.00F + phaseDown[phase]); break;
+					case "P3": v.Value = new Vector2(48.50F + phaseLeft[phase], 21.00F + phaseDown[phase]); break;
+					case "P4": v.Value = new Vector2(72.50F + phaseRight[phase], 21.00F + phaseDown[phase]); break;
+					case "P5": v.Value = new Vector2(72.10F + phaseRight[phase], 27.50F + phaseUp[phase]); break;
+					case "P6": v.Value = new Vector2(48.80F + phaseLeft[phase], 27.50F + phaseUp[phase]); break;
+					case "P7": v.Value = new Vector2(55.50F + (phaseLeft[phase]/2), 25.00F + phaseUp[phase]); break;
+					case "P8": v.Value = new Vector2(64.50F + (phaseRight[phase]/2), 25.00F + phaseUp[phase]); break;
+				}
+			}
+		}
+	}
 }
--- ModMenu.cs
+++ ModMenu.cs
@@ -3,33 +3,35 @@ using Modding;
 
 namespace Abskoth
 {
-    public partial class Abskoth : IMenuMod
-    {
-        public bool ToggleButtonInsideMenu => false;
+	public partial class Abskoth : IMenuMod
+	{
+		public bool ToggleButtonInsideMenu => true;
 
-        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
-        {
-            List<IMenuMod.MenuEntry> entries = new List<IMenuMod.MenuEntry>();
+		public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+		{
+			List<IMenuMod.MenuEntry> entries = new List<IMenuMod.MenuEntry>();
 
-            entries.Add(new IMenuMod.MenuEntry
-            {
-                Name = "Number of Markoths",
-                Description = "# of Markoths in the Absrad fight. This changes nothing when mid-fight.",
-                Values = new string[] { "1", "2", "3", "4" },
-                Saver = (i) => GlobalSaveData.numMarkoths = i + 1,
-                Loader = () => GlobalSaveData.numMarkoths > 0 ? GlobalSaveData.numMarkoths - 1 : 0
-            });
+			entries.Add((IMenuMod.MenuEntry)toggleButtonEntry);
+			
+			entries.Add(new IMenuMod.MenuEntry
+			{
+				Name = "Number of Markoths",
+				Description = "# of Markoths in the Absrad fight. This changes nothing when mid-fight.",
+				Values = new string[] { "1", "2", "3", "4" },
+				Saver = (i) => GlobalSaveData.numMarkoths = i + 1,
+				Loader = () => GlobalSaveData.numMarkoths > 0 ? GlobalSaveData.numMarkoths - 1 : 0
+			});
 
-            entries.Add(new IMenuMod.MenuEntry
-            {
-                Name = "Spawn Markoth's Nail Shots",
-                Description = "Toggle whether Markoth can spawn Nail Shots in the fight.",
-                Values = new string[] { "On", "Off" },
-                Saver = (i) => GlobalSaveData.withNail = i == 0,
-                Loader = () => GlobalSaveData.withNail ? 0 : 1
-            });
+			entries.Add(new IMenuMod.MenuEntry
+			{
+				Name = "Spawn Markoth's Nail Shots",
+				Description = "Toggle whether Markoth can spawn Nail Shots in the fight.",
+				Values = new string[] { "On", "Off" },
+				Saver = (i) => GlobalSaveData.withNail = i == 0,
+				Loader = () => GlobalSaveData.withNail ? 0 : 1
+			});
 
-            return entries;
-        }
-    }
+			return entries;
+		}
+	}
 }
